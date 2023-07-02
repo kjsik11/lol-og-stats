a60061a3487b0ed716885e6236d482586476b0fa
jongsik <kjsik11@gmail.com>
2023-07-02 14:19:54 +0900

    remove console.log
    update next version

--- src/components/custom/BuildOGImage.tsx
+++ src/components/custom/BuildOGImage.tsx
@@ -9,7 +9,6 @@ type Props = {
 };
 
 export default function BuildOGImage({ summonerInfo, matchInfoList }: Props) {
-  console.log('matchinfo-check', matchInfoList);
   return (
     <div
       style={{
